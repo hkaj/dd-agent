2a1bb90241a6206ddb4bd47b51dedabdefe1d6e0
Haissam Kaj <haissam.kaj@datadoghq.com>
2015-06-11 18:58:15 -0400

    improve exit status code

--- tests/circleci.sh
+++ tests/circleci.sh
@@ -2,13 +2,17 @@
 
 i=0
 tests=()
+exit_code=0
 IFS=',' read -ra flavors <<< "$CIRCLECI_FLAVORS"
 for flavor in $flavors
 do
   if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
   then
     rake ci:run_circle[$flavor]
+    if [ $exit_code -ne 0 ] || [ $? -ne 0] ; then
+      exit_code=1
+    fi
   fi
   ((i++))
 done
-
+exit $exit_code
